8acc85306d6788937c2c25978644a473e6a30142
Mateusjr07 <mateuswolf07@gmail.com>
2025-07-02 16:28:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+const botao = document.querySelector("#botao");
+    botao.style.background="blue";
+    botao.style.color="white";
+const veja = document.querySelector("#veja");
+    botao.addEventListener("click",e =>{
+        veja.innerHTML="Se estiver se sentindo desmotivado ou sentindo que não é bom o suficiente, incendeie o seu coração, enxugue as lágrimas e siga em frente."})
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
